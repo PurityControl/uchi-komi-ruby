e01b04cab6522a23237647fe05da776dbb1f4a2c
Craig Ferry <PurityControl@users.noreply.github.com>
2015-01-24 20:28:24 +0000

    passing tests for stored_value?

--- problems/rq/28-madlibs/ichi/lib/madlibs.rb
+++ problems/rq/28-madlibs/ichi/lib/madlibs.rb
@@ -3,12 +3,25 @@ class Madlibs
 
   def initialize madlib_string
     @madlib_string = madlib_string
+    @memoized_placeholders = Hash.new
   end
 
   def store_value col_del_str
+    key, value = col_del_str.split(':')
+    memoize_placeholder key, value
   end
 
   def stored_value? key
-    true
+    placeholder(key) != nil
+  end
+
+  private
+  # should these go into their own class and be enumerable
+  def memoize_placeholder key, value
+    @memoized_placeholders[key] = value
+  end
+
+  def placeholder key
+    @memoized_placeholders[key]
   end
 end
