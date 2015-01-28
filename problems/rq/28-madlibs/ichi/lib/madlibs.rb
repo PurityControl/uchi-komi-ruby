8f87310ff7f1731498cc61ab643cac5907669d25
Craig Ferry <PurityControl@users.noreply.github.com>
2015-01-28 08:52:20 +0000

    map placeholders to values for sprintf and print_madlib function

--- problems/rq/28-madlibs/ichi/lib/madlibs.rb
+++ problems/rq/28-madlibs/ichi/lib/madlibs.rb
@@ -1,12 +1,35 @@
 class Madlibs
   attr_reader :madlib_string, :placeholder_list
-  attr_accessor :madlib_strf
+  attr_accessor :madlib_strf, :value_list
 
   def initialize madlib_string
     @madlib_string = madlib_string
     @memoized_placeholders = Hash.new
     @madlib_strf = ""
     @placeholder_list = Array.new
+    @value_list = Array.new
+  end
+
+  def print_madlib
+    sprintf madlib_strf, *value_list
+  end
+
+  def process_placeholders
+    self.value_list = placeholder_list.map {|ph| process_placeholder(ph) }
+  end
+
+  def process_placeholder placeholder
+    if named_placeholder? placeholder
+      store_value placeholder
+    elsif stored_value? placeholder
+      get_placeholder placeholder
+    else
+      value_from_user placeholder
+    end
+  end
+
+  def append_to_value_list value
+    @value_list << value
   end
 
   def store_value col_del_str
@@ -16,14 +39,14 @@ class Madlibs
   end
 
   def stored_value? key
-    placeholder(key) != nil
+    get_placeholder(key) != nil
   end
 
   def named_placeholder? str
     /:/ =~ str
   end
 
-  def placeholder key
+  def get_placeholder key
     @memoized_placeholders[key]
   end
 
