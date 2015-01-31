42ccc255df23daecabed0d4a8b0d362bda39feb3
Craig Ferry <PurityControl@users.noreply.github.com>
2015-01-31 18:08:03 +0000

    implement solution using enumerable

--- problems/euler/0002-even-fibonacci-numbers/ichi/lib/even_fib.rb
+++ problems/euler/0002-even-fibonacci-numbers/ichi/lib/even_fib.rb
@@ -1,5 +1,22 @@
-module EvenFib
-  module_function
-  def sum_to limit
+class EvenFib
+  include Enumerable
+  attr_accessor :limit
+
+  def initialize limit
+    @limit = limit
+  end
+
+  def each
+    a,b = 1,2
+    yield a if a < limit
+    while (b < limit) do
+      yield b
+      a,b = b, a + b
+    end
+  end
+
+  def self.sum_to limit
+    evenfib = EvenFib.new limit
+    evenfib.select{ |num| num.even? }.reduce(:+)
   end
 end
