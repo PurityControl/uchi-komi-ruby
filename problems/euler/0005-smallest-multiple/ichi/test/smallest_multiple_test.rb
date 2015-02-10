3b91cdc0cab074181c59b23fe465de82b2aa20f9
Craig Ferry <PurityControl@users.noreply.github.com>
2015-02-10 06:53:09 -0800

    removed skip from tests and new unit test and put times of calc in notes

--- problems/euler/0005-smallest-multiple/ichi/test/smallest_multiple_test.rb
+++ problems/euler/0005-smallest-multiple/ichi/test/smallest_multiple_test.rb
@@ -18,13 +18,19 @@ class TestSmallestMultiple < MiniTest::Unit::TestCase
     refute smallest_multiple.all_factors? 2521
   end
 
+  def test_all_factors_20
+    smallest_multiple = SmallestMultiple.new 20
+    assert smallest_multiple.all_factors? 232792560
+    refute smallest_multiple.all_factors? 232792561
+  end
+
   def test_smallest_multiple_for_1_to_10
-    skip
     smallest_multiple = SmallestMultiple.new 10
     assert_equal 2520, smallest_multiple.smallest_multiple
   end
 
   def test_smallest_multiple_for_1_to_20
+    #skip
     smallest_multiple = SmallestMultiple.new 20
     assert_equal 232792560, smallest_multiple.smallest_multiple
   end
