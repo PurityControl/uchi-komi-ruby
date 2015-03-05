96d51d4b1d1d76c53158d28f95e349ae05ab017e
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-05 10:58:51 +0000

    add Enumerable and Forwardable functionality into grid for array behaviour

--- problems/euler/0011-largest-product-in-grid/ichi/lib/product_in_grid.rb
+++ problems/euler/0011-largest-product-in-grid/ichi/lib/product_in_grid.rb
@@ -1,4 +1,10 @@
+require 'forwardable'
 class Grid
+  include Enumerable
+  extend Forwardable
+
+  def_delegators :grid, :each, :each_with_index, :[]
+
   def initialize nums, length, depth
     nums = nums.split()
     if nums.length != length * depth
