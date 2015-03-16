f72d1401df84a4a55da8e131427c88a0be2d83dc
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-16 08:12:49 -0700

    add skeleton for lcd numbers project

---
+++ problems/rq/14-lcd-numbers/ichi/test/lcd_numbers_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'rake'
+
+class TestLcdNumbers < MiniTest::Unit::TestCase
+  def setup
+    #write setup stuff here
+  end
+
+  def teardown
+    #write teardown stuff here
+  end
+
+  def test_new_project_exists
+    flunk "write me - I'm lonely"
+  end
+
+end
