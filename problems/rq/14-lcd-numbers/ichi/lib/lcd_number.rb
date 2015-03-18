125350ae4c1c483ced417c3086fd792934147431
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-18 13:50:40 -0700

    lcd number class implemented

--- problems/rq/14-lcd-numbers/ichi/lib/lcd_number.rb
+++ problems/rq/14-lcd-numbers/ichi/lib/lcd_number.rb
@@ -1,30 +1,61 @@
 class LcdNumber
   def initialize number
+    @number = number
     @size = 1
-    @top, @middle, @bottom, @upper_vertical, @lower_vertical = ""
   end
 
   def render
-    "    \n  | \n  | \n    \n  | \n  | \n    \n"
+    render_top
+    render_upper_vertical
+    render_middle
+    render_lower_vertical
+    render_bottom
+    [top, upper_vertical, middle, lower_vertical, bottom].flatten
   end
 
   private
 
-  attr_reader :size
+  attr_reader :size, :number
+  attr_accessor :top, :middle, :bottom, :upper_vertical, :lower_vertical
 
   def render_top
-    top << " " << size.times{number.char_for_position(1)} << " "
+    self.top = render_horizontal(LcdOutput::TOP)
   end
 
   def render_upper_vertical
+    self.upper_vertical = render_vertical LcdOutput::UPPER_LEFT,
+      LcdOutput::UPPER_RIGHT
   end
 
   def render_middle
+    self.middle = render_horizontal LcdOutput::MIDDLE
   end
 
   def render_lower_vertical
+    self.lower_vertical = render_vertical LcdOutput::LOWER_LEFT,
+      LcdOutput::LOWER_RIGHT
   end
 
   def render_bottom
+    self.bottom = render_horizontal LcdOutput::BOTTOM
+  end
+
+  def render_horizontal position
+    tmp = " "
+    size.times do
+      tmp << LcdOutput.char_for(position, number)
+    end
+    tmp << "  "
+  end
+
+  def render_vertical left, right
+    rows = []
+    tmp = ""
+    size.times do
+      tmp << LcdOutput.char_for(left ,number) << " " <<
+        LcdOutput.char_for(right, number) << " "
+      rows << tmp
+    end
+    rows
   end
 end
--- problems/rq/14-lcd-numbers/ichi/test/lcd_number_test.rb
+++ problems/rq/14-lcd-numbers/ichi/test/lcd_number_test.rb
@@ -13,15 +13,54 @@ class TestLcdNumber < MiniTest::Unit::TestCase
   end
 
   def test_render_1
-    lcd = LcdNumber.new 1
-    skip("need to clarify api")
+    lcd = LcdNumber.new LcdMapping.map_for(1)
+    #skip("need to clarify api")
     assert_equal lcd.render, ["    ", "  | ", "    ", "  | ", "    "]
   end
 
   def test_render_2
-    lcd = LcdNumber.new 2
-    skip("need to clarify api")
+    lcd = LcdNumber.new LcdMapping.map_for(2)
     assert_equal lcd.render, [" _  ", "  | ", " _  ", "|   ", " _  "]
+  end
+
+  def test_render_3
+    lcd = LcdNumber.new LcdMapping.map_for(3)
+    assert_equal lcd.render, [" _  ", "  | ", " _  ", "  | ", " _  "]
+  end
+
+  def test_render_4
+    lcd = LcdNumber.new LcdMapping.map_for(4)
+    assert_equal lcd.render, ["    ", "| | ", " _  ", "  | ", "    "]
+  end
+
+  def test_render_5
+    lcd = LcdNumber.new LcdMapping.map_for(5)
+    assert_equal lcd.render, [" _  ", "|   ", " _  ", "  | ", " _  "]
+  end
+
+  def test_render_6
+    lcd = LcdNumber.new LcdMapping.map_for(6)
+    assert_equal lcd.render, [" _  ", "|   ", " _  ", "| | ", " _  "]
+  end
+
+  def test_render_7
+    lcd = LcdNumber.new LcdMapping.map_for(7)
+    assert_equal lcd.render, [" _  ", "  | ", "    ", "  | ", "    "]
+  end
+
+  def test_render_8
+    lcd = LcdNumber.new LcdMapping.map_for(8)
+    assert_equal lcd.render, [" _  ", "| | ", " _  ", "| | ", " _  "]
+  end
+
+  def test_render_9
+    lcd = LcdNumber.new LcdMapping.map_for(9)
+    assert_equal lcd.render, [" _  ", "| | ", " _  ", "  | ", " _  "]
+  end
+
+  def test_render_0
+    lcd = LcdNumber.new LcdMapping.map_for(0)
+    assert_equal lcd.render, [" _  ", "| | ", "    ", "| | ", " _  "]
   end
 
 end
