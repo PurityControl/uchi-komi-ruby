01049b4ee64f1585ae815feb4057b4827430ed34
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-27 05:40:51 -0700

    represent hash constants as binary literals for improved clarity

--- problems/rq/14-lcd-numbers/ichi/lib/lcd_output.rb
+++ problems/rq/14-lcd-numbers/ichi/lib/lcd_output.rb
@@ -1,12 +1,12 @@
 class LcdOutput
   Positions = {
-    top:            64,
-    upper_left:     32,
-    upper_right:    16,
-    middle:         8,
-    lower_left:     4,
-    lower_right:    2,
-    bottom:         1,
+    top:            0b1000000,
+    upper_left:     0b0100000,
+    upper_right:    0b0010000,
+    middle:         0b0001000,
+    lower_left:     0b0000100,
+    lower_right:    0b0000010,
+    bottom:         0b0000001,
   }
 
   def self.output_at? posn_key, mask
