fe5507e37a24564aa9ddb1b8ddb437a258ac3df5
Craig Ferry <craig@puritycontrol.net>
2015-10-04 15:58:15 +0100

    Passing base cases up to the count of three

--- problems/euler/0017-number-letter-counts/ichi/lib/number_letter_counts.rb
+++ problems/euler/0017-number-letter-counts/ichi/lib/number_letter_counts.rb
@@ -1,2 +1,25 @@
 class NumberLetterCounts
+  include Enumerable
+
+  def initialize total
+    @total = total
+    @letter_from_number = {
+      1 => "one",
+      2 => "two",
+      3 => "three"
+    }
+  end
+
+  def number_letter_counts
+    map(&:length).reduce(:+)
+  end
+
+  def each
+    1.upto(total) do |num|
+      yield letter_from_number[num]
+    end
+  end
+
+  private
+  attr_accessor :total, :letter_from_number
 end
--- problems/euler/0017-number-letter-counts/ichi/test/number_letter_counts_test.rb
+++ problems/euler/0017-number-letter-counts/ichi/test/number_letter_counts_test.rb
@@ -2,9 +2,27 @@ require "minitest/autorun"
 require "number_letter_counts"
 
 class NumberLetterCountsTest < Minitest::Test
+  def test_to_one
+    nlc_to 1
+    assert_count 3
+  end
 
-  def test_class_instantiated
-    instance = NumberLetterCounts.new
-    refute_nil instance
+  def test_to_two
+    nlc_to 2
+    assert_count 6
+  end
+
+  def test_to_three
+    nlc_to 3
+    assert_count 11
+  end
+
+  private
+  def nlc_to upper
+    @nlc = NumberLetterCounts.new upper
+  end
+
+  def assert_count expected
+    assert_equal @nlc.number_letter_counts, expected
   end
 end
