34c59139e57fe14f148281bb7b2c1022f3416a17
Craig Ferry <craig@puritycontrol.net>
2015-10-05 09:44:53 +0100

    passing base cases up to the count of ten.

--- problems/euler/0017-number-letter-counts/ichi/lib/number_letter_counts.rb
+++ problems/euler/0017-number-letter-counts/ichi/lib/number_letter_counts.rb
@@ -3,11 +3,18 @@ class NumberLetterCounts
 
   def initialize total
     @total = total
-    @letter_from_number = {
-      1 => "one",
-      2 => "two",
-      3 => "three"
-    }
+    @letter_from_number = [
+      [10, "ten"],
+      [9, "nine"],
+      [8, "eight"],
+      [7, "seven"],
+      [6, "six"],
+      [5, "five"],
+      [4, "four"],
+      [3, "three"],
+      [2, "two"],
+      [1, "one"]
+    ]
   end
 
   def number_letter_counts
@@ -16,10 +23,21 @@ class NumberLetterCounts
 
   def each
     1.upto(total) do |num|
-      yield letter_from_number[num]
+      yield number_to_word num
     end
   end
 
   private
   attr_accessor :total, :letter_from_number
+
+  def number_to_word num
+    description = ""
+    letter_from_number.each do |item, word|
+      if num / item > 0
+        description <<  " #{word}"
+        num -= item
+      end
+    end
+    description.strip
+  end
 end
--- problems/euler/0017-number-letter-counts/ichi/test/number_letter_counts_test.rb
+++ problems/euler/0017-number-letter-counts/ichi/test/number_letter_counts_test.rb
@@ -17,12 +17,32 @@ class NumberLetterCountsTest < Minitest::Test
     assert_count 11
   end
 
+  def test_to_ten
+    nlc_to 10
+    assert_count 39
+  end
+
+  def test_1_is_one
+    nlc_to 1
+    assert_equal "one", @nlc.send(:number_to_word, 1)
+  end
+
+  def test_2_is_two
+    nlc_to 1
+    assert_equal "two", @nlc.send(:number_to_word, 2)
+  end
+
+  def test_3_is_three
+    nlc_to 1
+    assert_equal "three", @nlc.send(:number_to_word, 3)
+  end
+
   private
   def nlc_to upper
     @nlc = NumberLetterCounts.new upper
   end
 
   def assert_count expected
-    assert_equal @nlc.number_letter_counts, expected
+    assert_equal expected, @nlc.number_letter_counts
   end
 end
